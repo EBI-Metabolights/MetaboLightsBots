af60172e93fc8509f7b4013a5a5f1b5a96be7a26
cmartin <cmartin@ebi.ac.uk>
2022-11-15 10:04:20 +0000

    removed sandbox.py

--- MetCompoundBot/sandbox.py
+++
@@ -1,8 +0,0 @@
-from MetCompoundBot import utils
-
-complist = utils.fetchMetaboLightsCompoundsList()
-print(len(complist))
-
-studylist = utils.fetchMetaboLightsStudiesList()
-print(len(studylist))
-
